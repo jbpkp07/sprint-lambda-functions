1dc51d711ecdb70b729d1a0cdc17dc850dc9e18c
jbpkp07 <jeremy.w.barnes@gmail.com>
2020-05-30 00:45:23 -0700

    fixed .env intructions

--- functions/newAsperaPackageArrival/start-dev-compiler.sh
+++ functions/newAsperaPackageArrival/start-dev-compiler.sh
@@ -1,15 +1,19 @@
-tput setab 0;
-tput bold;
-
 lambdaFuncName=$(basename "$(dirname "$0")");
 
 echo -e '\033]2;'TypeScript Compiler:  [$lambdaFuncName]'\007';
 
+tput setab 0;
+tput bold;
 tput setaf 7;
+
 printf "  Running TypeScript Compiler:  [";
+
 tput setaf 6;
+
 printf "$lambdaFuncName";
+
 tput setaf 7;
+
 printf "]\n\n";
 
 rm -r -f build;
--- functions/newAsperaPackageArrival/start-dev-function.sh
+++ functions/newAsperaPackageArrival/start-dev-function.sh
@@ -1,16 +1,21 @@
-tput setab 0;
-tput bold;
-
 lambdaFuncName=$(basename "$(dirname "$0")");
 
 echo -e '\033]2;'Lambda Function:  [$lambdaFuncName]'\007';
 
+tput setab 0;
+tput bold;
 tput setaf 7;
+
 printf "  Running Lambda Function:  [";
-tput setaf 6; 
+
+tput setaf 6;
+
 printf "$lambdaFuncName";
+
 tput setaf 7;
+
 printf "]\n\n";
+
 
 cd build;
 
